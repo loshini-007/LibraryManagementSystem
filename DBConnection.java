502854d32d39219829eb10ead04c2159cd219c93
loshini-007 <717824y122@kce.ac.in>
2026-02-23 22:36:02 +0530

    Create DBConnection.java
    Added DBConnection.java

---
+++ DBConnection.java
@@ -0,0 +1,12 @@
+package com.library.util;
+import java.sql.Connection; import java.sql.DriverManager; public class DBConnection {
+public static Connection getConnection() { try {
+Class.forName("com.mysql.cj.jdbc.Driver"); return DriverManager.getConnection(
+"jdbc:mysql://localhost:3306/library_db", "root",
+"password"
+);
+} catch (Exception e) { e.printStackTrace(); return null;
+}
+}
+}
+ 
